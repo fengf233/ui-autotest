7a72190be03f0d2ff4d55b0b1f187b12ca9689d5
fengf233 <38374173+fengf233@users.noreply.github.com>
2019-04-16 18:02:48 +0800

    Update base_page.py

--- basepage/base_page.py
+++ basepage/base_page.py
@@ -20,18 +20,28 @@ class Basepage():
         e.g.
             self.find(By.ID,"test")
         
-        返回webelement对象,可以使用其所有方法
+        返回webelement对象,可以使用其所有方法,常用的有:
+        - webelement.click()
+        - webelement.clear()
+        - webelement.send_keys(str)
+        - webelement.submit()
+        - webelement.size
+        - webelement.text
+        - webelement.get_attribute(name)
+        - webelement.is_displayed()
+        - webelement.is_selected()
+        ....
+
         e.g.
             self.find(By.ID,"test").click()
         '''
         try:
+            log.info("查找元素: %s" %key[1])
             return self.driver.find_element(*key)
-        except NoSuchElementException:
-            log.error("没有找到元素: %s" %key[1])
+        except Exception as e:
+            log.error("没有找到元素: %s 异常为:\n %s" %(key[1],e))
             raise
-        except TimeoutException:
-            log.error("查找元素超时: %s" %key[1])
-            raise
+        
 
     def finds(self,*key):
         '''
@@ -41,12 +51,10 @@ class Basepage():
         返回webelement对象列表,可以使用其所有方法
         '''
         try:
+            log.info("查找元素: %s" %key[1])
             return self.driver.find_elements(*key)
-        except NoSuchElementException:
-            log.error("没有找到元素: %s" %key[1])
-            raise
-        except TimeoutException:
-            log.error("查找元素超时: %s" %key[1])
+        except Exception as e:
+            log.error("没有找到元素: %s 异常为:\n %s" %(key[1],e))
             raise
     
     #界面等待
@@ -71,7 +79,7 @@ class Basepage():
         def presence(self,*key,timeout=2,frequency=0.5):
             try:
                 log.info("等待元素出现: %s" %key[1])
-                return WebDriverWait(self.driver,timeout,poll_frequency=frequency).until(EC.presence_of_element_located((*key)))
+                return WebDriverWait(self.driver,timeout,poll_frequency=frequency).until(EC.presence_of_element_located(key))
             except TimeoutException:
                 log.error("等待元素出现超时: %s" %key[1])
                 raise
@@ -79,7 +87,7 @@ class Basepage():
         def visibility(self,*key,timeout=2,frequency=0.5):
             try:
                 log.info("等待元素显示: %s" %key[1])
-                return WebDriverWait(self.driver,timeout,poll_frequency=frequency).until(EC.visibility_of_element_located((*key)))
+                return WebDriverWait(self.driver,timeout,poll_frequency=frequency).until(EC.visibility_of_element_located(key))
             except TimeoutException:
                 log.error("等待元素显示超时: %s" %key[1])
                 raise
@@ -87,7 +95,7 @@ class Basepage():
         def invisibility(self,*key,timeout=2,frequency=0.5):
             try:
                 log.info("等待元素消失: %s" %key[1])
-                return WebDriverWait(self.driver,timeout,poll_frequency=frequency).until(EC.invisibility_of_element_located((*key)))
+                return WebDriverWait(self.driver,timeout,poll_frequency=frequency).until(EC.invisibility_of_element_located(key))
             except TimeoutException:
                 log.error("等待元素消失超时: %s" %key[1])
                 raise
@@ -122,6 +130,12 @@ class Basepage():
 
     #界面操作
 
+    def window_size(self,*size):
+        if size:
+            self.driver.set_window_size(*size)
+        else:
+            self.driver.maximize_window()
+
     def get_url(self):
         log.info("当前页面的url: %s" % self.driver.current_url)
         return self.driver.current_url
@@ -134,9 +148,9 @@ class Basepage():
         '''
         e.g. self.click(By.ID,"test")
         '''
-        log.info('点击元素：%s' %key[1])
         try:
             self.find(*key).click()
+            log.info('点击元素：%s' %key[1])
         except:
             log.error("无法点击元素")
             raise
@@ -153,6 +167,37 @@ class Basepage():
         except:
             log.error("输入内容失败")
             raise
+    
+    def clear(self,*key):
+        '''
+        e.g. self.clear(By.ID,"test")
+        '''
+        log.info('清除元素内容：%s' %key[1])
+        try:
+            self.find(*key).clear()
+        except:
+            log.error("无法清除元素内容")
+            raise
+    
+    def switch_to_window(self,handle):
+        self.driver.switch_to_window(handle)
+        log.info('切换到[%s]窗口'%self.get_title())
+
+    def switch_to_frame(self,*key):
+        '''
+        切换框架,直接输入iframe的name或id属性，或者传入定位的元素
+
+        e.g.
+        self.switch_to_frame("if")
+        self.switch_to_frame(By.ID,"if")
+        '''
+        if len(key) == 2:
+            xf = self.find(*key)
+            self.driver.switch_to_frame(xf)
+            log.info("切换到[%s]frame表单"%key[1])
+            return ''
+        self.driver.switch_to_frame(key)
+        log.info("切换到[%s]frame表单"%key)
 
     def quit(self):
         self.driver.quit()
@@ -227,6 +272,9 @@ class Basepage():
         return EC.visibility_of_element_located(key)(self.driver)
 
     def url_contains(self,url):
+        '''
+        判断给定url是否包含在当前url中
+        '''
         return EC.url_contains(url)(self.driver)
 
     def alert_is_present(self):
@@ -235,6 +283,9 @@ class Basepage():
         弹出框显示则返回alert对象或false
         '''
         return EC.alert_is_present()(self.driver)
+    
+    def switch_alert(self):
+        return self.driver.switch_to_alert()
 
     def click_alert(self):
         try:
@@ -274,7 +325,13 @@ class Basepage():
     def get_cookies(self):
         return self.driver.get_cookies()
 
+    def add_cookie(self,cookie_dict):
+        self.driver.add_cookie(cookie_dict)
+        
     def save_cookies(self):
+        '''
+        保存cookies为json文件
+        '''
         cookies = self.driver.get_cookies()
         cookies_path = os.path.join(os.path.dirname(os.path.dirname(__file__))+ '/report/'+type(self).__name__+'/cookies/')
         if os.path.exists(cookies_path):
@@ -285,11 +342,29 @@ class Basepage():
         with open(cookies_file,'w') as f:
             json.dump(cookies,f)
         log.info('cookies保存在:%s'%cookies_path)
+        return cookies_file
 
-    def add_cookies(self):
-        cookies_file = os.path.join(os.path.dirname(os.path.dirname(__file__))+ '/report/'+type(self).__name__+'/cookies/') +'cookies.json'
-        with open(cookies_file,'w') as f :
+    def add_cookies(self,cookies_file=''):
+        '''
+        从cookies.json文件添加所有cookies
+        '''
+        if not cookies_file:
+            cookies_file = os.path.join(os.path.dirname(os.path.dirname(__file__))+ '/report/'+type(self).__name__+'/cookies/') +'cookies.json'
+        with open(cookies_file,'r') as f :
             cookies = json.load(f)
+            # self.driver.delete_all_cookies()
             for cookie in cookies:
                 self.driver.add_cookie(cookie)
         log.info('cookies加载成功:%s'%cookies_file)
+
+
+    #行为链,直接返回action api,自己定义更方便
+
+    def action(self):
+        '''
+        行为链操作，以perform()提交操作
+        e.g.
+        action = self.action()
+        action.move_to_element(element).click(element).perform()
+        '''
+        return ActionChains(self.driver)
