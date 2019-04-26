32ec2a12e65dd3bbbdda984ee27cff3391b9bba1
fengf233 <38374173+fengf233@users.noreply.github.com>
2019-04-26 13:36:28 +0800

    调整结构
    调整结构&增加HTML报告部分

--- common/base_page.py
+++ common/base_page.py
@@ -1,4 +1,4 @@
-from basepage import mylog
+from common import mylog
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
